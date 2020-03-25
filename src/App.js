b929f37788660614e315ab3d37d81be81040e1ee
zeGolem <zegolem1383@outlook.fr>
2020-03-25 21:06:37 +0100

    Added a basic homepage with a test video.
    This video is loaded from your computer directly ! It uses the
    ReactPlayer suggested by @not_pickle_rick and this seems to work just
    fine (at least for now).
    I am not that familiar with React best practices for now, so please edit
    any of the code that I got wrong.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cybermate</h1>
+        <h2>Home to all things tech !</h2>
       </header>
+      <div id="App-body">
+        <h2 className="Title">Test video :</h2>
+        <center><VideoPlayer url='testvideo.mp4' 
+          subtitle="This is a video from the YouTube channel 'Webdriver Torso' 
+          which AFAIK is/was used by YT for testing their video quality"/>
+        </center>
+      </div>
+    </div>
+  );
+}
+
+function VideoPlayer(props) {
+  return (
+    <div className="VideoPlayer">
+      <ReactPlayer url={props.url} controls muted></ReactPlayer>
+      <p><i>{props.subtitle}</i></p>
     </div>
   );
 }
